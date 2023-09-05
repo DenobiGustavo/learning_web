4b1e75f16bcafbac4be0fd910d99e5241e8a8f34
GustavoGamer <gustavodenobi2022@gmail.com>
2023-09-05 10:31:22 -0300

    exercício de função em js

---
+++ Exercícios JS/funcao2.js
@@ -0,0 +1,16 @@
+const imprimirSoma = function (a, b){
+
+    console.log(a + b)
+
+}
+
+const soma = (a, b) => {
+
+    return a + b
+}
+
+console.log(soma(234, 233))
+
+const subtracao = (a, b) => a - b
+console.log(subtracao(3, 4))
+
